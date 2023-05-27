58d2dd294d06a63ddb11fe0b0bfb8d4e880142aa
Jáchym Janoušek <jachym.janousek@woltair.cz>
2023-05-27 20:58:57 +0200

    refactor sun position

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import SunCalc from "suncalc3";
 
-let times = SunCalc.getSunTimes(new Date(), 51.5, -0.1);
-
-console.log(times.sunrise);
+// get sun position
+const sunPos = SunCalc.getPosition(new Date(), 51.5, -0.1); // London
